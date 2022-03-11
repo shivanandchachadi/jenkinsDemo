23257e65b0ae81c19c639bd814fc536b38d69aa2
Shivanand.chachadi <shivu.2007@gmail.com>
2022-03-11 14:21:34 +0530

    commit form second branch

--- src/main/java/com/example/shiv/demo/shiv/Application.java
+++ src/main/java/com/example/shiv/demo/shiv/Application.java
@@ -32,6 +32,15 @@ public class Application {
 		
 		System.out.println("set values "+ set);
 		
+		for(int i=0;i<10;i++)
+		{
+			for (int j=0;j<=i;j++)
+			{
+				System.out.print(" * ");
+				
+			}
+			System.out.println(" ");
+		}
 		
 		
 		
